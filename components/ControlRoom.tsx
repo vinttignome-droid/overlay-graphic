b1ee3b7cad15fee1061fff7c8f78debbcb5da02d
Jonne <vinttignome@gmail.com>
2026-03-17 21:41:00 +0200

    fix: poista duplikaatti matchesStorageKey-määrittely

--- components/ControlRoom.tsx
+++ components/ControlRoom.tsx
@@ -16,7 +16,6 @@ interface ControlRoomProps {
 export default function ControlRoom({ sport, onLogout }: ControlRoomProps) {
   // ...existing code...
   const teamsStorageKey = `ligr:${sport}:teams`;
-  const matchesStorageKey = `ligr:${sport}:matches`;
   const playersStorageKey = `ligr:${sport}:players`;
 
   // Kuuntele localStorage-muutoksia (esim. polling tai toinen selain)
